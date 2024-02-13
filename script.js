203dcb0e43e290fbe54a107a685a10e8e86ee66f
dylannsw <79353359+dylannsw@users.noreply.github.com>
2024-02-14 02:12:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ document.getElementById('yes-button').addEventListener('click', function() {
     document.getElementById('celebration-container').style.display = 'block';
 });
 
-
 document.getElementById('no-button').addEventListener('mouseover', function() {
     const container = document.querySelector('.container');
-    const maxX = container.clientWidth - this.clientWidth;
-    const maxY = container.clientHeight - this.clientHeight;
+    const maxX = window.innerWidth - this.clientWidth;
+    const maxY = window.innerHeight - this.clientHeight;
 
     const newX = Math.random() * maxX;
     const newY = Math.random() * maxY;
